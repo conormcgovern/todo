763911f0752437c6664a16c030d0a39fe907bfc2
Conor McGovern <c.mcgovern014@gmail.com>
2020-10-25 19:09:11 -0400

    fix App import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Home';
+import App from './App';
 import netlifyIdentity from 'netlify-identity-widget';
 
 import loadTheme from './loadTheme';
